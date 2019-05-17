18c4d2461c9ff5a5d692f369728a6c16caa552a9
hkahng92@gmail.com <hkahng92@gmail.com>
2019-05-16 20:11:58 -0400

    Updates

---
+++ precache-manifest.3b1dd6b07283fcb0b82baaaae73162c1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15172e9323b35a9d99240a02f04235a2",
+    "url": "/myportfoliosite/index.html"
+  },
+  {
+    "revision": "d4b97bc11e0bbfbbe0b1",
+    "url": "/myportfoliosite/static/css/2.45ca5176.chunk.css"
+  },
+  {
+    "revision": "75537e179df3b3990753",
+    "url": "/myportfoliosite/static/css/main.8804fffe.chunk.css"
+  },
+  {
+    "revision": "d4b97bc11e0bbfbbe0b1",
+    "url": "/myportfoliosite/static/js/2.4d826b15.chunk.js"
+  },
+  {
+    "revision": "75537e179df3b3990753",
+    "url": "/myportfoliosite/static/js/main.18f3631f.chunk.js"
+  },
+  {
+    "revision": "5752a9d77b071f257625",
+    "url": "/myportfoliosite/static/js/runtime~main.f47761cb.js"
+  },
+  {
+    "revision": "2605cc69bdd1120db791949d79fee575",
+    "url": "/myportfoliosite/static/media/myPhoto.2605cc69.jpg"
+  },
+  {
+    "revision": "d7277b3b0e28f1d3829e0de40c4f8843",
+    "url": "/myportfoliosite/static/media/myPhoto2.d7277b3b.jpg"
+  },
+  {
+    "revision": "b09f0cad3622fa3523bd7520d9cd8a95",
+    "url": "/myportfoliosite/static/media/myPhoto3.b09f0cad.jpg"
+  }
+]);
